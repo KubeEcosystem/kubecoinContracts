397066400964d8dddced2e2947740db788d6e94a
Giuliano <95681714+GiuBlockchainDEV@users.noreply.github.com>
2024-05-27 05:00:19 +0200

    Update index.tsx

--- staking/frontend/demo_app/routes/index.tsx
+++ staking/frontend/demo_app/routes/index.tsx
@@ -1,12 +1,10 @@
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 import SPA from "../islands/SPA.tsx";
 
 export default function Home() {
   const count = useSignal(3);
   return (
     <div>
-      <Counter count={count} />
       <SPA />
     </div>
   );
